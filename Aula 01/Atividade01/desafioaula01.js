3591ee21836ee2547c895c4a8099847330da0765
João Henrique <112053980+jaohenriqu3@users.noreply.github.com>
2023-03-06 23:41:29 -0300

    Update desafioaula01.js

--- Aula 01/Atividade01/desafioaula01.js
+++ Aula 01/Atividade01/desafioaula01.js
@@ -5,7 +5,7 @@
 
 var altura, sexo;
 var somaalturahomens = 0;
-var quantidamulheres = 0;
+var quantidademulheres = 0;
 var maioraltura = 0;
 var menoraltura = 0;  
 
@@ -14,21 +14,20 @@ for (var i = 1; i <= 15; i++) {
     sexo = prompt(`Digite o sexo da ${i}ª pessoa (M ou F):`);
 
 if (altura > maioraltura) {
-    maiorAltura = altura; }
+    maioraltura = altura; }
   
 if (altura < menoraltura) {
-    menorAltura = altura; }
+    menoraltura = altura; }
   
 if (sexo === 'M') {
-    somaAlturaHomens += altura;}
-
+    somaalturahomens += altura;}
 else if (sexo === 'F') {
-    quantidadeMulheres++; }
+    quantidademulheres++; }
 }  
 
-const mediaalturaHomens = somaalturaHomens / 15;
+const mediaalturaHomens = somaalturahomens / 15;
 
 console.log ("A maior altura do grupo é", maiorAltura) 
 console.log ("A menor altura do grupo é", menorAltura) 
 console.log ("A media de altura dos homens é, " ) 
-console.log ("A quantidade de mulheres pesquisadas foi de" , quantidademulheres)
+console.log ("A quantidade de mulheres pesquisadas foi de" , quantidademulheres)  
