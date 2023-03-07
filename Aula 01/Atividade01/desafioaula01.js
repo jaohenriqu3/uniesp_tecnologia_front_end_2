9c44015d71344d0f71be0cf3d6810d102ea1d8a5
João Henrique <112053980+jaohenriqu3@users.noreply.github.com>
2023-03-07 18:28:12 -0300

    Update desafioaula01.js

--- Aula 01/Atividade01/desafioaula01.js
+++ Aula 01/Atividade01/desafioaula01.js
@@ -1,33 +1,37 @@
-// algoritmo que obtenha dados de altura e o sexo (masculino ou feminino) de 15 pessoas e apresente os seguintes resultados:
-// a maior e menor altura do grupo
-// a media de altura dos homens 
-// numero de mulheres 
-
-var altura, sexo;
+var altura;
+var sexo;
 var somaalturahomens = 0;
 var quantidademulheres = 0;
 var maioraltura = 0;
 var menoraltura = 0;  
 
+// Perguntas para o usuario 
 for (var i = 1; i <= 15; i++) {
-    altura = prompt(`Digite a altura da ${i}ª pessoa:`);
-    sexo = prompt(`Digite o sexo da ${i}ª pessoa (M ou F):`);
+    altura = prompt(`Digite a altura:`);
+    sexo = prompt(`Digite o sexo (M ou F):`);
 
+// Obter maior altura
 if (altura > maioraltura) {
     maioraltura = altura; }
-  
+
+// Obter menor altura  
 if (altura < menoraltura) {
     menoraltura = altura; }
-  
+
+// Obter quantidade de homens e somar  
 if (sexo === 'M') {
-    somaalturahomens += altura;}
+    somaalturahomens += altura;} 
+    
+// Obter quantidade de mulheres    
 else if (sexo === 'F') {
     quantidademulheres++; }
 }  
 
+// Calcular media de homens 
 const mediaalturaHomens = somaalturahomens / 15;
 
-console.log ("A maior altura do grupo é", maiorAltura) 
-console.log ("A menor altura do grupo é", menorAltura) 
+// Imprimir resultados 
+console.log ("A maior altura do grupo é", maioraltura) 
+console.log ("A menor altura do grupo é", menoraltura) 
 console.log ("A media de altura dos homens é, " ) 
 console.log ("A quantidade de mulheres pesquisadas foi de" , quantidademulheres)  
